2dbcf2cf8d80ce97fb462d714c1d1fce9f7fb254
pengzhengfa <13656694002@163.com>
2020-07-12 10:41:34 +0800

    Add order Engineering

---
+++ mitbook-example/mitbook-order/src/main/java/com/mitbook/MitbookOrderApplication.java
@@ -0,0 +1,13 @@
+package com.mitbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitbookOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitbookOrderApplication.class, args);
+    }
+
+}
---
+++ mitbook-example/mitbook-order/src/test/java/com/mitbook/MitbookOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitbookOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
